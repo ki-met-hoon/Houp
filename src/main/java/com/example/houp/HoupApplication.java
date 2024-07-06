dd8cb27cc8766ccaae5d6558658a955aba1a1f72
KimSehoon <shggm2000@gmail.com>
2024-07-06 13:10:36 +0900

    #1 feat: 스프링 부트 프로젝트 초기화
    들어간 초기 기술:
    * Sprirng web
    * Spring data JPA
    * devtools
    * lombok
    * H2
    * MySql Driver
    
    .gitIgnore 파일 추가 내용:
    * intellij
    * vscode
    * macOS
    * windows

---
+++ src/main/java/com/example/houp/HoupApplication.java
@@ -0,0 +1,13 @@
+package com.example.houp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoupApplication.class, args);
+    }
+
+}
