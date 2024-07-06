e3bfe0147d508b476cfdff7c1863709d1f0a7154
KimSehoon <shggm2000@gmail.com>
2024-07-06 14:32:38 +0900

    #2 feat: NursingFacilities Entity 생성
    - @Column에 nullable = false 처리 하지 않고 @NotNull 사용
     - 관련 레퍼런스: https://kafcamus.tistory.com/15

---
+++ src/main/java/com/example/houp/nursingfacility/NursingFacilities.java
@@ -0,0 +1,60 @@
+package com.example.houp.nursingfacility;
+
+import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class NursingFacilities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long nursingFacilityId;
+
+    @Column(length = 128)
+    @NotNull
+    private String encryptedCode;
+
+    @Column(length = 64)
+    @NotNull
+    private String facilityName;
+
+    private int typeCode;
+
+    @Column(length = 16)
+    @NotNull
+    private String typeCodeName;
+
+    private int cityCode;
+
+    @Column(length = 8)
+    @NotNull
+    private String cityCodeName;
+
+    @Column(length = 16)
+    @NotNull
+    private String phoneNumber;
+
+    private double coordinateX;
+
+    private double coordinateY;
+
+    @Column(length = 8)
+    @NotNull
+    private String city;
+
+    @Column(length = 8)
+    @NotNull
+    private String district;
+
+    @Column(length = 16)
+    @NotNull
+    private String street;
+
+    @Column(length = 8)
+    @NotNull
+    private String buildingNumber;
+
+    @Column(length = 32)
+    private String detailAddress;
+}
