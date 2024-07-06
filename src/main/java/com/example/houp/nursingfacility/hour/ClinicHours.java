576e8d0521e091c149ae148151a7201ad62e77c2
KimSehoon <shggm2000@gmail.com>
2024-07-06 14:41:10 +0900

    #2 feat: ClinicHours Entity 생성

---
+++ src/main/java/com/example/houp/nursingfacility/hour/ClinicHours.java
@@ -0,0 +1,26 @@
+package com.example.houp.nursingfacility.hour;
+
+import com.example.houp.nursingfacility.NursingFacilities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Entity
+public class ClinicHours {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clinicHourId;
+
+    @ManyToOne
+    @JoinColumn
+    private NursingFacilities nursingFacilities;
+
+    @Column(length = 8)
+    @NotNull
+    private String dayOfWeek;
+
+    private int startTime;
+
+    private int endTime;
+}
