f007358a708839f05a604fbd5e013caa07db8048
KimSehoon <shggm2000@gmail.com>
2024-07-06 14:48:17 +0900

    #2 feat: FacilityDetails 생성

---
+++ src/main/java/com/example/houp/nursingfacility/detail/FacilityDetails.java
@@ -0,0 +1,33 @@
+package com.example.houp.nursingfacility.detail;
+
+import com.example.houp.nursingfacility.NursingFacilities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Setter
+@Entity
+public class FacilityDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private NursingFacilities nursingFacilities;
+
+    @Column(length = 8)
+    @NotNull
+    private String parkingAvailable;
+
+    @Column(length = 16)
+    private String lunchTime;
+
+    @Column(length = 8)
+    private String closedOnSunday;
+
+    @Column(length = 8)
+    private String closedOnHoliday;
+}
