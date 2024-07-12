839f92830a05a5dab807a266d1b72fe9490cce10
KimSehoon <shggm2000@gmail.com>
2024-07-12 14:29:00 +0900

    #4 feat: ExcelParerController 구현

--- src/main/java/com/example/houp/support/excelparser/controller/ExcelParerController.java
+++ src/main/java/com/example/houp/support/excelparser/controller/ExcelParerController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,8 +13,8 @@ public class ExcelParerController {
     private final ExcelParerService excelParerService;
 
     @GetMapping("/upload")
-    public ResponseEntity<Void> uploadExcel(@RequestParam String fileName) {
-        excelParerService.readExcel(fileName);
+    public ResponseEntity<Void> uploadExcel() {
+        excelParerService.readExcel("C:\\Users\\kwoon\\houp data.xlsx");
         return ResponseEntity.ok().build();
     }
 }
