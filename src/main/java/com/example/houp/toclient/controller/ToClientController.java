a310091c657ceb8344e3d11e87304ce682280900
KimSehoon <shggm2000@gmail.com>
2024-08-10 22:55:22 +0900

    #14 refacotr: return 값을 ResponEntity로 변경

--- src/main/java/com/example/houp/toclient/controller/ToClientController.java
+++ src/main/java/com/example/houp/toclient/controller/ToClientController.java
@@ -6,11 +6,11 @@ import com.example.houp.toclient.dto.PredictionResponse;
 import com.example.houp.toclient.dto.UserInfoRequest;
 import com.example.houp.toclient.service.ToClientService;
 import com.example.houp.tocomwel.service.ToComwelService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ToClientController {
@@ -18,13 +18,13 @@ public class ToClientController {
     private final ToComwelService toComwelService;
 
     @PostMapping("/disease-predictions")
-    PredictionResponse handleUserInfo(@RequestBody UserInfoRequest userInfoRequest) {
-        return toClientService.getPredictedDiseaseInfo(userInfoRequest);
+    public ResponseEntity<PredictionResponse> handleUserInfo(@Valid @RequestBody UserInfoRequest userInfoRequest) {
+        return ResponseEntity.ok(toClientService.getPredictedDiseaseInfo(userInfoRequest));
     }
 
     @GetMapping("/worker-compensation-infos")
-    JudgementDocumentResponse handleDiseaseInfo(@RequestParam("diseaseName") String diseaseName, @RequestParam("jobKind") String jobKind, @RequestParam("diseaseKind") String diseaseKind) {
+    public ResponseEntity<JudgementDocumentResponse> handleDiseaseInfo(@RequestParam("diseaseName") String diseaseName, @RequestParam("jobKind") String jobKind, @RequestParam("diseaseKind") String diseaseKind) {
         CaseExamples relatedDiseaseJudgments = toComwelService.getDiseaseDisagnosisReport(diseaseName, jobKind, diseaseKind);
-        return toClientService.getPredictedDisagnosisReport(relatedDiseaseJudgments);
+        return ResponseEntity.ok(toClientService.getPredictedDisagnosisReport(relatedDiseaseJudgments));
     }
 }
