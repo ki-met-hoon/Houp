81664236c057f8f2a3d02c0335d95b86a214ac6d
KimSehoon <shggm2000@gmail.com>
2024-10-23 22:15:16 +0900

    refactor: 질병 유형 유효성 검사 예외 원인 구체화

---
+++ src/main/java/com/example/houp/toai/support/exception/DiseaseKindExceptionByAi.java
@@ -0,0 +1,10 @@
+package com.example.houp.toai.support.exception;
+
+public class DiseaseKindExceptionByAi extends RuntimeException {
+
+    private static final String MESSAGE = "AI로부터 받은 질병유형이 올바르지 못합니다.";
+
+    public DiseaseKindExceptionByAi() {
+        super(MESSAGE);
+    }
+}
