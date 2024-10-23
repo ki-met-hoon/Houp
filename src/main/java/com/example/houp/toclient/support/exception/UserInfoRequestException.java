448e2fcc2f152c25716c32363dd7b97e1cd0807e
KimSehoon <shggm2000@gmail.com>
2024-10-23 22:21:52 +0900

    feat: 질병 진단 API 요청 중 사용자 정보 유효성 검사 예외 처리

---
+++ src/main/java/com/example/houp/toclient/support/exception/UserInfoRequestException.java
@@ -0,0 +1,12 @@
+package com.example.houp.toclient.support.exception;
+
+import com.example.houp.advice.BadRequestException;
+
+public class UserInfoRequestException extends BadRequestException {
+
+    private static final String MESSAGE = "질병 진단 API 요청 중 전달하는 유저 정보에 오류가 발생했습니다.";
+
+    public UserInfoRequestException() {
+        super(MESSAGE);
+    }
+}
