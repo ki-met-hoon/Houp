21d3cddf1f1dab78ad684ac0e7094b7825dd1db2
KimSehoon <shggm2000@gmail.com>
2024-10-28 18:07:47 +0900

    #19 refactor: 사용자 정보 유효성 검사 로직 추가

--- backend/SpringWebflux/src/main/java/com/example/houp/toclient/handler/ToClientHandler.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/toclient/handler/ToClientHandler.java
@@ -1,5 +1,6 @@
 package com.example.houp.toclient.handler;
 
+import com.example.houp.support.util.RequestValidator;
 import com.example.houp.toclient.dto.UserInfoRequest;
 import com.example.houp.toclient.service.ToClientService;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,11 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class ToClientHandler {
     private final ToClientService toClientService;
+    private final RequestValidator<UserInfoRequest> validator;
 
     public Mono<ServerResponse> handleUserInfo(ServerRequest request) {
         return request.bodyToMono(UserInfoRequest.class)
+                .doOnNext(validator::validate)
                 .flatMap(toClientService::getPredictedDiseaseInfo)
                 .flatMap(predictionResponse -> ServerResponse.ok().bodyValue(predictionResponse));
     }
