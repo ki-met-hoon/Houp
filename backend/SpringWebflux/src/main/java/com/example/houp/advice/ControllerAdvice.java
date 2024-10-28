bf62a01472663341edc3257005125873c85e21ff
KimSehoon <shggm2000@gmail.com>
2024-10-28 18:23:33 +0900

    #21 refactor: return 값을 Non-Blocking 형식으로 변환

--- backend/SpringWebflux/src/main/java/com/example/houp/advice/ControllerAdvice.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/advice/ControllerAdvice.java
@@ -4,23 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+    public Mono<ResponseEntity<ErrorResponse>> handleNotFoundException(NotFoundException e) {
+        return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage())));
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    public Mono<ResponseEntity<ErrorResponse>> handleBadRequestException(BadRequestException e) {
+        return Mono.just(ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage())));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+    public Mono<ResponseEntity<ErrorResponse>> handleRuntimeException(RuntimeException e) {
 //        return ResponseEntity.internalServerError().body(new ErrorResponse("서버에 알 수 없는 문제가 발생했습니다."));
-        return ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage()));
+        return Mono.just(ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage())));
     }
 }
