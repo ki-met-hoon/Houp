cc1fd7038fc6f8b3fc9c071bebc41bcdb18ccb4d
KimSehoon <shggm2000@gmail.com>
2024-11-07 00:21:36 +0900

    refacotor: getPredictedDisagnosisReport method
    - 비동기 I/O 형식으로 구조 변경
    - AI에게 산재보험처리확률을 받는 API를 webClient로 비동기식으로 요청

--- backend/SpringWebflux/src/main/java/com/example/houp/toclient/service/ToClientService.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/toclient/service/ToClientService.java
@@ -1,12 +1,15 @@
 package com.example.houp.toclient.service;
 
 import com.example.houp.support.properties.toai.ToAiProperties;
+import com.example.houp.support.util.KindValidator;
+import com.example.houp.tocomwel.dto.CaseExamples;
+import com.example.houp.toclient.support.exception.JobKindExceptionByAi;
+import com.example.houp.toclient.dto.JudgementDocumentResponse;
 import com.example.houp.toclient.dto.PredictionResponse;
+import com.example.houp.toclient.dto.ReportToClient;
 import com.example.houp.toclient.dto.UserInfoRequest;
-import com.example.houp.toclient.support.exception.DiseaseResponseByAiException;
-import com.example.houp.toclient.support.exception.DiseaseResponseBySeverException;
-import com.example.houp.toclient.support.exception.PredictionResponseParsingException;
-import com.example.houp.toclient.support.exception.UserInfoRequestException;
+import com.example.houp.toclient.support.exception.*;
+import com.example.houp.tocomwel.support.exception.DiseaseKindExceptionByUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -19,46 +22,79 @@ import reactor.core.publisher.Mono;
 public class ToClientService {
 
     private final ToAiProperties toAiProperties;
-    private final WebClient diseaseWebClient;
+    private final WebClient toAiWebClient;
 
     public Mono<PredictionResponse> getPredictedDiseaseInfo(UserInfoRequest userInfoRequest) {
-        return diseaseWebClient
+        return toAiWebClient
                 .post()
                 .uri(toAiProperties.disease().url())
                 .bodyValue(userInfoRequest)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, response -> {
                     if (response.statusCode().equals(HttpStatus.BAD_REQUEST))
-                        return Mono.error(new UserInfoRequestException());
+                        return Mono.error(new DiseaseRequestException());
                     else
                         return Mono.error(new DiseaseResponseBySeverException());
                 })
                 .onStatus(HttpStatusCode::is5xxServerError, response -> Mono.error(new DiseaseResponseByAiException()))
                 .bodyToMono(PredictionResponse.class)
-                .onErrorResume(error -> Mono.error(new PredictionResponseParsingException()));
+                .doOnNext(predictionResponse -> validate(predictionResponse.jobKind(), predictionResponse.diseaseKind()));
     }
 
-//    public ReportToClient getPredictedDisagnosisReport(CaseExamples caseExamples) {
-//        JudgementDocumentResponse judgementDocumentResponse = toAIService.getJudgementDocument(caseExamples);
-//        return convertToReportToClient(judgementDocumentResponse, caseExamples);
-//    }
-//
-//    private ReportToClient convertToReportToClient(JudgementDocumentResponse response, CaseExamples caseExamples) {
-//        return new ReportToClient(
-//                response.approvalProbability(),
-//                response.result(),
-//                new ReportToClient.CaseExamples(
-//                        convertCase(response.caseExamples().firstCase(), caseExamples),
-//                        convertCase(response.caseExamples().secondCase(), caseExamples)
-//                )
-//        );
-//    }
-//
-//    private ReportToClient.Case convertCase(JudgementDocumentResponse.Case similarCase, CaseExamples caseExamples) {
-//        return new ReportToClient.Case(
-//                caseExamples.caseExamples().get(similarCase.id()).reviewResult(),
-//                similarCase.summary(),
-//                caseExamples.caseExamples().get(similarCase.id()).judgmentDocument()
-//        );
-//    }
+    private void validate(String jobKind, String diseaseKind) {
+        validateJobKind(jobKind);
+        validateDiseaseKind(diseaseKind);
+    }
+
+    private void validateJobKind(String jobKind) {
+        if (!KindValidator.isValidJobKind(jobKind)) {
+            throw new JobKindExceptionByAi();
+        }
+    }
+
+    private void validateDiseaseKind(String diseaseKind) {
+        if (!KindValidator.isValidDiseaseKind(diseaseKind)) {
+            throw new DiseaseKindExceptionByUser();
+        }
+    }
+
+    public Mono<ReportToClient> getPredictedDisagnosisReport(CaseExamples caseExamples) {
+        return getJudgementDocument(caseExamples)
+                .map(judgeDoc -> convertToReportToClient(judgeDoc, caseExamples));
+    }
+
+    public Mono<JudgementDocumentResponse> getJudgementDocument(CaseExamples caseExamples) {
+        return toAiWebClient
+                .post()
+                .uri(toAiProperties.report().url())
+                .bodyValue(caseExamples)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> {
+                    if (response.statusCode().equals(HttpStatus.BAD_REQUEST))
+                        return Mono.error(new JudgeDocRequestException());
+                    else
+                        return Mono.error(new JudgeDocResponseBySeverException());
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, response -> Mono.error(new JudgeDocResponseByAiException()))
+                .bodyToMono(JudgementDocumentResponse.class);
+    }
+
+    private ReportToClient convertToReportToClient(JudgementDocumentResponse response, CaseExamples caseExamples) {
+        return new ReportToClient(
+                response.approvalProbability(),
+                response.result(),
+                new ReportToClient.CaseExamples(
+                        convertCase(response.caseExamples().firstCase(), caseExamples),
+                        convertCase(response.caseExamples().secondCase(), caseExamples)
+                )
+        );
+    }
+
+    private ReportToClient.Case convertCase(JudgementDocumentResponse.Case similarCase, CaseExamples caseExamples) {
+        return new ReportToClient.Case(
+                caseExamples.caseExamples().get(similarCase.id()).reviewResult(),
+                similarCase.summary(),
+                caseExamples.caseExamples().get(similarCase.id()).judgmentDocument()
+        );
+    }
 }
