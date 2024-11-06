f56402dd4608d6979e9fff31f72be7608b47321a
KimSehoon <shggm2000@gmail.com>
2024-11-07 00:23:37 +0900

    feat: handleDiseaseInfo method 구현
    - client에게 userDiseaseInfo를 받음
    - 해당 정보를 토대로 근로복지공단에서 관련 질병판정서를 조회함
    - 질병판정서와 유저 정보를 LLM에게 전달해서 산재처리확률, 결론, 인정/불인정 사례를 받아 응답

--- backend/SpringWebflux/src/main/java/com/example/houp/toclient/handler/ToClientHandler.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/toclient/handler/ToClientHandler.java
@@ -1,24 +1,39 @@
 package com.example.houp.toclient.handler;
 
 import com.example.houp.support.util.RequestValidator;
+import com.example.houp.toclient.dto.UserDiseaseInfoRequest;
 import com.example.houp.toclient.dto.UserInfoRequest;
 import com.example.houp.toclient.service.ToClientService;
+import com.example.houp.tocomwel.service.ToComwelService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class ToClientHandler {
+
     private final ToClientService toClientService;
-    private final RequestValidator<UserInfoRequest> validator;
+    private final ToComwelService toComwelService;
+    private final RequestValidator<UserInfoRequest> userInfoValidator;
+    private final RequestValidator<UserDiseaseInfoRequest> userDiseaseInfoValidator;
 
     public Mono<ServerResponse> handleUserInfo(ServerRequest request) {
         return request.bodyToMono(UserInfoRequest.class)
-                .doOnNext(validator::validate)
+                .doOnNext(userInfoValidator::validate)
                 .flatMap(toClientService::getPredictedDiseaseInfo)
                 .flatMap(predictionResponse -> ServerResponse.ok().bodyValue(predictionResponse));
+    }
+
+    public Mono<ServerResponse> handleDiseaseInfo(ServerRequest request) {
+        return request.bodyToMono(UserDiseaseInfoRequest.class)
+                .doOnNext(userDiseaseInfoValidator::validate)
+                .flatMap(toComwelService::getDiseaseDisagnosisReport)
+                .flatMap(toClientService::getPredictedDisagnosisReport)
+                .flatMap(reportToClient -> ServerResponse.ok().bodyValue(reportToClient));
     }
 }
