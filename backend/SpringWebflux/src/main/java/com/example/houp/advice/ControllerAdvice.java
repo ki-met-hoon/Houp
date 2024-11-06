62f59022993db43d7c80a8344719ba3f47345c69
KimSehoon <shggm2000@gmail.com>
2024-11-07 00:13:32 +0900

    refactor: Runtime Error 처리 방식 변경
    - 서버에 로그를 찍고 클라이언트에게는 서버의 자세한 설명보단 오류의 출처만 알려줌

--- backend/SpringWebflux/src/main/java/com/example/houp/advice/ControllerAdvice.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/advice/ControllerAdvice.java
@@ -1,11 +1,13 @@
 package com.example.houp.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
@@ -21,7 +23,7 @@ public class ControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public Mono<ResponseEntity<ErrorResponse>> handleRuntimeException(RuntimeException e) {
-//        return ResponseEntity.internalServerError().body(new ErrorResponse("서버에 알 수 없는 문제가 발생했습니다."));
-        return Mono.just(ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage())));
+        log.debug(e.getMessage());
+        return Mono.just(ResponseEntity.internalServerError().body(new ErrorResponse("서버에 알 수 없는 문제가 발생했습니다.")));
     }
 }
