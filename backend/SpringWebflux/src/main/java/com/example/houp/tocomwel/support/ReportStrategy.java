3adfe55ad941da4f0eae1f6584b7009f2938b860
KimSehoon <shggm2000@gmail.com>
2024-11-07 00:14:45 +0900

    refactor: getReports method return 값을 비동기 형식으로 변경

--- backend/SpringWebflux/src/main/java/com/example/houp/tocomwel/support/ReportStrategy.java
+++ backend/SpringWebflux/src/main/java/com/example/houp/tocomwel/support/ReportStrategy.java
@@ -1,8 +1,10 @@
-//package com.example.houp.tocomwel.support;
-//
-//import com.example.houp.tocomwel.dto.ReportToObject;
-//
-//public interface ReportStrategy {
-//
-//    ReportToObject getReports(ToComwelCaller caller, String serviceKey, String pageNo, String numOfRows);
-//}
+package com.example.houp.tocomwel.support;
+
+import com.example.houp.tocomwel.dto.ReportToObject;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public interface ReportStrategy {
+
+    Mono<ReportToObject> getReports(WebClient reportWebClient, String pageNo, String numOfRows);
+}
